740b7c3c8c0c60ec72c85e98b68c3e148547eec6
valltw <valltw@DESKTOP-K27397S.localdomain>
2021-07-27 13:39:02 -0500

    Number concatenation with numbers and displays it

--- script.js
+++ script.js
@@ -1,1 +1,77 @@
-console.log('holiwi');
+let cuenta =0;
+console.log(cuenta, typeof cuenta);
+console.log(cuenta, typeof cuenta);
+console.log(cuenta.includes(0));
+
+function addone(){    
+    cuenta = cuenta + "1";
+    console.log(cuenta.slice(1));
+    document.getElementById("display").innerHTML=cuenta.slice(1);
+}
+function addtwo(){
+   
+    cuenta = cuenta + "2";
+    console.log(cuenta.slice(1));
+    document.getElementById("display").innerHTML=cuenta.slice(1);
+    document.getElementById("display").innerHTML=cuenta.slice(1);
+}
+function addthree(){
+    
+    cuenta = cuenta + "3";
+    console.log(cuenta.slice(1));
+    document.getElementById("display").innerHTML=cuenta.slice(1);
+}
+function addfour(){
+    
+    cuenta = cuenta + "4";
+    console.log(cuenta.slice(1));
+    document.getElementById("display").innerHTML=cuenta.slice(1);
+}
+function addfive(){
+    
+    cuenta = cuenta + "5";
+    console.log(cuenta.slice(1));
+    document.getElementById("display").innerHTML=cuenta.slice(1);
+}
+function addsix(){
+    
+    cuenta = cuenta + "6";
+    console.log(cuenta.slice(1));
+    document.getElementById("display").innerHTML=cuenta.slice(1);
+}
+function addseven(){
+    
+    cuenta = cuenta + "7";
+    console.log(cuenta.slice(1));
+    document.getElementById("display").innerHTML=cuenta.slice(1);
+}
+function addeight(){
+
+    cuenta = cuenta + "8";
+    console.log(cuenta.slice(1));
+    document.getElementById("display").innerHTML=cuenta.slice(1);
+}
+function addnine(){
+    
+    cuenta = cuenta + "9";
+    console.log(cuenta.slice(1));
+    document.getElementById("display").innerHTML=cuenta.slice(1);
+}
+function addzero(){
+    if(cuenta>0){
+
+        cuenta = cuenta + "0";
+        console.log(cuenta.slice(1));
+        document.getElementById("display").innerHTML=cuenta.slice(1);
+
+    }
+    }
+// function addpoint(){ 
+//     if(cuenta.search(".") === -1)
+//     {
+//         cuenta = cuenta + ".";
+//     console.log(cuenta.slice(1));
+//     document.getElementById("display").innerHTML=cuenta.slice(1);
+//     }
+    
+// }
