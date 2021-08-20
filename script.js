99e916cbd1f8735d86b133e01387ef0dddad3d18
valltw <valltw@DESKTOP-HSVV5GB.localdomain>
2021-08-20 00:41:57 -0500

    Added notebook to write operations

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let cuenta ="0";
-let cuenta2=0;
-let operator="0";
-let tado=0;
-let redo=0;
+let cuenta = "0";
+let cuenta2 = 0;
+let operator = "0";
+let tado = 0;
+let redo = 0;
 
 // const nombre = 'Victor'
 // const apellidoPaterno = 'Servin'
@@ -14,48 +14,48 @@ let redo=0;
 
 function printValueToDisplay() {
     if (cuenta.startsWith("0.")) {
-        document.getElementById("display").innerHTML=cuenta;
+        document.getElementById("display").innerHTML = cuenta;
         return;
     }
     document.getElementById("display").innerHTML = cuenta.slice(1);
 }
 
 function addNumber(number) {
-    if(redo === 0){
+    if (redo === 0) {
         // cuenta = `${ cuenta }${ number }`;
         // cuenta = cuenta + number; // 1 + 1 = 2, '1' + '1' = '11'
         cuenta += `${number}`; // cuenta = cuenta + `${number}` // 5 += 2 => 7
         printValueToDisplay();
         return;
-    }  
+    }
     AC();
     cuenta += `${number}`;
     printValueToDisplay();
 }
 
-function AC(){
-    cuenta="0";
-    cuenta2=0;
-    operator="0";
-    tado=0;
-    redo=0;
-    document.getElementById("display").innerHTML="0";
+function AC() {
+    cuenta = "0";
+    cuenta2 = 0;
+    operator = "0";
+    tado = 0;
+    redo = 0;
+    document.getElementById("display").innerHTML = "0";
 }
-function addzero(){
-    if(cuenta != "0" && redo === 0){
+
+function addzero() {
+    if (cuenta != "0" && redo === 0) {
         cuenta += "0";
         printValueToDisplay();
         return;
     }
-    cuenta="0";
-    redo =0;
-    document.getElementById("display").innerHTML=0;
-    }
-    
-function addpoint(){ 
-    if(cuenta.includes(".") === false)
-    {
-        if (redo === 1){
+    cuenta = "0";
+    redo = 0;
+    document.getElementById("display").innerHTML = 0;
+}
+
+function addpoint() {
+    if (cuenta.includes(".") === false) {
+        if (redo === 1) {
             AC();
             cuenta = cuenta + ".";
             printValueToDisplay();
@@ -66,77 +66,95 @@ function addpoint(){
         return;
     }
 }
-function operador(op){
-    if(redo === 1){
-        cuenta2=parseFloat(tado);
-        cuenta="0";
-        operator= op;
-        document.getElementById("display").innerHTML="0";
-        redo=0;
+
+function operador(op) {
+    if (redo === 1) {
+        cuenta2 = parseFloat(tado);
+        cuenta = "0";
+        operator = op;
+        document.getElementById("display").innerHTML = "0";
+        redo = 0;
         return;
     }
-    if(cuenta === "0"){
+    if (cuenta === "0") {
         return;
     }
-    cuenta2=parseFloat(cuenta);
-    cuenta="0";
-    operator=op;
-    document.getElementById("display").innerHTML="0";
+    cuenta2 = parseFloat(cuenta);
+    cuenta = "0";
+    operator = op;
+    document.getElementById("display").innerHTML = "0";
 }
 
-function DEL(){
-    if(cuenta.length>2 && redo === 0){
-        cuenta=cuenta.slice(0,-1);
+function DEL() {
+    if (cuenta.length > 2 && redo === 0) {
+        cuenta = cuenta.slice(0, -1);
         printValueToDisplay();
         return;
     }
     AC();
 }
-function plusmin(){
-    if(redo === 1){
-        cuenta=-1 * parseFloat(tado);
-        redo=0;
-        document.getElementById("display").innerHTML=cuenta;
+
+function plusmin() {
+    if (redo === 1) {
+        cuenta = -1 * parseFloat(tado);
+        redo = 0;
+        document.getElementById("display").innerHTML = cuenta;
         return;
     }
-    cuenta=-1 * cuenta;
-    document.getElementById("display").innerHTML=cuenta;
+    cuenta = -1 * cuenta;
+    document.getElementById("display").innerHTML = cuenta;
 }
-function desition(opera){
-    switch(opera){
-        case "+" :
-            tado= cuenta2 + parseFloat(cuenta); 
-            document.getElementById("display").innerHTML=tado;
+
+function desition(opera) {
+    switch (opera) {
+        case "+":
+            tado = cuenta2 + parseFloat(cuenta);
+            document.getElementById("display").innerHTML = tado;
             break;
-        case "-" :
-            tado= cuenta2 - parseFloat(cuenta); 
-            document.getElementById("display").innerHTML=tado;
+        case "-":
+            tado = cuenta2 - parseFloat(cuenta);
+            document.getElementById("display").innerHTML = tado;
             break;
-        case "*" :
-            tado= cuenta2 * parseFloat(cuenta); 
-            document.getElementById("display").innerHTML=tado;
+        case "*":
+            tado = cuenta2 * parseFloat(cuenta);
+            document.getElementById("display").innerHTML = tado;
             break;
-        case "/" :
-            if(cuenta === '0'){
-                document.getElementById("display").innerHTML='Math Error';
-                break;   
+        case "/":
+            if (cuenta === '0') {
+                document.getElementById("display").innerHTML = 'Math Error';
+                break;
             }
-            tado= cuenta2 / parseFloat(cuenta); 
-            document.getElementById("display").innerHTML=tado;
+            tado = cuenta2 / parseFloat(cuenta);
+            document.getElementById("display").innerHTML = tado;
             break;
         default:
-        }
+    }
 }
 
 
-function res(){
-    if(redo != 1){
-        redo=1;
+function res() {
+    if (redo != 1) {
+        redo = 1;
         desition(operator);
+        printToPaper();
         return;
     }
-        cuenta2=tado;
-        desition(operator);
-        return;
+    cuenta2 = tado;
+    desition(operator);
+    printToPaper();
+    return;
 }
-// 30/07/21
+
+function printToPaper() {
+    if (cuenta === "0" || operator === "0") {
+        return;
+    }
+    let cuentita = cuenta.slice(1);
+    let list = document.getElementById("list");
+    list.innerHTML += `<li>${cuenta2}${operator}${cuentita} = ${tado}</li>`;
+}
+
+function clearbtn() {
+    let list = document.getElementById("list");
+    list.innerHTML="";
+}
